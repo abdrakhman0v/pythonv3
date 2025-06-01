510c25c69e5a3b23ceecef99e49b518291c58dd7
abdrakhman0v <abdrakhman0v.006@gmail.com>
2025-06-01 15:39:12 +0600

    Новые изменения

--- files/CRUD.py
+++ files/CRUD.py
@@ -1,43 +1,70 @@
 import json
 
 def create_product(new_product, file):
-    with open(file) as f:
-        products = json.load(f)
-        products.append(new_product)
+    try:
+        with open(file) as f:
+            products = json.load(f)       
+    except FileNotFoundError:
+        print(f"{file} - не найден. Создаю новый файл.")
+        products = []
+    products.append(new_product)
+
     with open(file, "w") as f:
         json.dump(products, f, indent=4)
     print("Вы успешно добавили товар!")
 
-# create_product({"id":1, "title":"nike", "price":1200},"shop_db.json")
-# create_product({"id":2, "title":"iPhone", "price":30000},"shop_db.json")
+create_product({"id":1, "title":"nike", "price":1200},"shop_db.json")
+create_product({"id":2, "title":"iPhone", "price":30000},"shop_db.json")
+create_product({"id":3, "title":"adidas", "price":500},"shop_db.json")
+create_product({"id":4, "title":"Samsung", "price":20000},"shop_db.json")
 
 def read_products(file):
     with open(file) as f:
         products = json.load(f)
         for product in products:
-            print(f"Товар: {product["title"]}, цена - {product["price"]} ")
+            print(f"Товар: {product['title']}, цена - {product['price']}$")
 
-# read_products("shop_db.json")
+read_products("shop_db.json")
 
 def read_product(id,file):
     with open(file) as f:
         products = json.load(f)
         product = next((p for p in products if p["id"] == id), "Неверный id")
-        print(product)
+        print(f"Товар - {product['title']}. Цена - {product["price"]}$")
 
-# read_product(2,"shop_db.json")
+read_product(2,"shop_db.json")
         
         
 
-def update_product(id,file):
+def update_product(id,key,new,file):
     with open(file) as f:
         products = json.load(f)
-        for product in products:
+    for product in products:
+        if product["id"] == id:
             product[key] = new
+            break
+    else:
+        print("Неверный id")
+        return   
     with open(file, "w") as f:
-        json.dump(product, f)
+        json.dump(products, f, indent=4)
+    print(f"Товар - {product['title']} обновлен")
+        
+update_product(1,"price", 500,"shop_db.json")
+update_product(3,"title", "ps","shop_db.json")
 
-update_product("title","mouse","shop_db.json")
+def delete_product(id,file):
+    with open(file) as f:
+        products = json.load(f)
+    for product in products:
+        if product["id"] == id:
+            products.remove(product)
+            break
+    else:
+        print("Неверный id")
+        return
+    with open(file,"w") as f:
+        json.dump(products,f,indent=4)
+    print(f"Товар - {product['title']} удален!")
 
-def delete_product(id, file):
-    ...
+delete_product(1,"shop_db.json")
--- hm/files.hm/json_hm.py
+++ hm/files.hm/json_hm.py
@@ -1,82 +1,83 @@
 import json
 
-#1
-# with open("data.json") as f:
-#     python_obj = json.load(f)
-#     print(python_obj)
+# 1
+with open("data.json") as f:
+    python_obj = json.load(f)
+    print(python_obj)
 
-#2
-# data_user = {
-#     "name":"Temirlan",
-#     "age":19,
-#     "email":"g1noma69@gmail.com"
-# }
+# 2
+data_user = {
+    "name":"Temirlan",
+    "age":19,
+    "email":"g1noma69@gmail.com"
+}
 
-# with open("user.json", "w") as f:
-#     json.dump(data_user, f, indent=4)
+with open("user.json", "w") as f:
+    json.dump(data_user, f, indent=4)
 
-#3
-# with open("string.json") as f:
-#     python_dict = json.load(f)
-#     print(python_dict["price"])
+# 3
+json_str = '{"product": "Laptop", "price": 1200, "in_stock": true}'
+python_dict = json.loads(json_str)
+print(python_dict["price"])
 
-#4
-# def read_product(id, file):
-#     with open(file) as f:
-#         products = json.load(f)
-#         product = next((p for p in products if p["id"] == id), "Неверный id")
-#         print(product)
+# 4
+def read_product(id, file):
+    with open(file) as f:
+        products = json.load(f)
+        product = next((p for p in products if p["id"] == id), "Неверный id")
+        print(product)
 
-# read_product(4,"products.json")
+read_product(4,"products.json")
 
-#5
-# def update_config(key,new,file):
-#     with open(file) as f:
-#         config = json.load(f)
-#         config[id] = new
-#     with open(file, "w") as f:
-#         json.dump(config, f)
-#     print("Настройки обновлены.")
+# 5
+def update_config(key,new,file):
+    with open(file) as f:
+        config = json.load(f)
+        config[key] = new
+    with open(file, "w") as f:
+        json.dump(config, f)
+    print("Настройки обновлены.")
      
-# update_config("volume",120,"config.json")
-# update_config("theme","dark","config.json")
+update_config("volume",120,"config.json")
+update_config("theme","dark","config.json")
 
-#6
-# def top_students(file):
-#     with open(file) as f:
-#         python_list = json.load(f)
-#     with open("top_students.json", "w") as f:
-#         filtered = [s for s in python_list if s["score"]>80]
-#         json.dump(filtered,f,indent=4)
+# 6
+def top_students(file):
+    with open(file) as f:
+        python_list = json.load(f)
+    with open("top_students.json", "w") as f:
+        filtered = [s for s in python_list if s["score"]>80]
+        json.dump(filtered,f,indent=4)
 
-# top_students("students.json")
+top_students("students.json")
 
-#7
-# with open("employees1.json") as f:
-#     python_list1 = json.load(f)
-# with open("employees2.json") as f:
-#     python_list2 = json.load(f)
-# with open("all_employees.json","w") as f:
-#     all_empl = python_list1 + python_list2 
-#     unique = []
-#     for item in all_empl:
-#         if item not in unique:
-#             unique.append(item)
-#     json.dump(unique,f,indent=4)
+# 7
+with open("employees1.json") as f:
+    python_list1 = json.load(f)
+with open("employees2.json") as f:
+    python_list2 = json.load(f)
+with open("all_employees.json","w") as f:
+    all_empl = python_list1 + python_list2 
+    unique = []
+    for item in all_empl:
+        if item not in unique:
+            unique.append(item)
+    json.dump(unique,f,indent=4)
     
-#8
+# 8
 from datetime import datetime
+
 def log_event(event, user_id):
     with open("log.json") as f:
         python_list = json.load(f)
-        now = datetime.now()
-        new_json = event
+        now = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+        new_text = {}
+        new_text["timestamp"] = now
+        new_text["event"] = event
+        new_text["user_id"] = user_id 
+        python_list.append(new_text)
+    with open("log.json", "w") as f:
+        json.dump(python_list, f, indent=4)
 
-
-        
-
-
-
-
-
-
+log_event("User log out", 777)
+log_event("User updeated profile", 123)
